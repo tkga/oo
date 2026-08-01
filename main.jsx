8ab9b8825ddca2199864b93870e76b253e97ef3a
tkga <tkgamer14260@gmail.com>
2026-08-01 13:50:21 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PokemonGoShop from "./PokemonGoShop.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PokemonGoShop />
+  </React.StrictMode>
+);
